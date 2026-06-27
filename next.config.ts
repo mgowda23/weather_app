2e57e249d4db3b2bdb89f41888e06c7c069d328e
mgowda23 <mgowda@binghamton.edu>
2026-06-27 16:23:01 -0400

    resolved workspace warning

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: "/Users/mithunvgowda/Projects/Final Projects/weather_app",
+  },
 };
 
 export default nextConfig;
